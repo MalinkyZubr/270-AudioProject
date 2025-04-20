9b9bafd0c04ef1c0896a45a363cdff52222a11f2
Dongliang-luo <151103156+Dongliang-luo@users.noreply.github.com>
2025-04-20 14:59:15 -0400

    Create adc_config.c
    Sets up ADC on Pico and collects 32 audio samples. no buffer yet.

---
+++ project/dong/adc_config.c
@@ -0,0 +1,23 @@
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+
+#define SAMPLE_RATE 8000 // Hz
+#define NUM_SAMPLES 32
+#define ADC_PIN 26
+
+uint16_t audio_samples[NUM_SAMPLES];
+
+void setup() {
+    adc_init();
+    adc_gpio_init(ADC_PIN);
+    adc_select_input(0); // ADC0
+}
+
+void sample_audio() {
+    uint32_t interval = 1000000 / SAMPLE_RATE; // microseconds
+    
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        audio_samples[i] = adc_read();
+        sleep_us(interval);
+    }
+}
